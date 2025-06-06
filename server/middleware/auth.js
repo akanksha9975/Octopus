45788173ff85199e0c751874177d50773329b6b0
akanksha9975 <akankshayadav9975@gmail.com>
2025-06-06 10:51:17 +0530

    new commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,14 +7,15 @@ const SECRET = process.env.SECRET;
 const auth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
-        const isCustomAuth = token.length < 500 
+        const isCustomAuth = token.length < 500   //checks whether token is custom token r google auth token
 
         let decodeData;
 
-        //If token is custom token do this
+       
+       
 
 
-        
+         //If token is custom token do this
         if(token && isCustomAuth) {
             decodeData = jwt.verify(token, SECRET)
 
